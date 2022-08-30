7583be62951a10279e471574998174b0a7ce35b0
Rafael Castillo <rafael@megabadvisors.com>
2022-08-30 11:27:07 -0400

    Initial commit

---
+++ lib/ps_auth_service.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ps_auth_service/version"
+
+module PsAuthService
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ps_auth_service_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe PsAuthService do
+  it "has a version number" do
+    expect(PsAuthService::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
